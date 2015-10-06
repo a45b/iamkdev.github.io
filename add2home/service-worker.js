b9e5eec75cce361a3964652cc9495d053afff142
Kamal dev Bhuyan <kamaldev.bhuyan@gmail.com>
2015-10-06 14:31:28 +0530

    testing

--- add2home/service-worker.js
+++ add2home/service-worker.js
@@ -25,10 +25,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('airhorner').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/index.html?homescreen=1',
-        '/?homescreen=1'
+        '/add2home'
       ]).then(function() {
         return self.skipWaiting();
       });
